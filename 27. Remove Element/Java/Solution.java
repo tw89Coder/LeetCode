f3f98f8dbbfd9ccac1729370eefe5bd4487cbab1
YHL <123497542+tw89Coder@users.noreply.github.com>
2024-07-21 23:10:26 +0800

    Add files via upload

--- 27. Remove Element/Java/Solution.java
+++ 27. Remove Element/Java/Solution.java
@@ -1,5 +1,15 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
+        int k = 0; // This will hold the count of non-val elements
         
+        // Iterate through the array
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i]; // Copy the non-val element to the front
+                k++; // Move the position for the next valid element
+            }
+        }
+        
+        return k; // k is the number of elements not equal to val
     }
 }
